4f65f6d112cd060d8f5419d4a348d12e8a04a603
Evaapostol <eugeniaapostol@hotmail.gr>
2019-05-13 05:26:42 +0300

    Add project files.

---
+++ ex1.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value1");
+            double value1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value2");
+            double value2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value3");
+            double value3 = double.Parse(Console.ReadLine());
+
+            double E = HeronFormula(value1, value2, value3);
+            Console.WriteLine("The solution is {0}:", E);
+        }
+        static double HeronFormula(double value1, double value2, double value3)
+        {
+
+
+            double sp;
+            double t;
+            double E;
+            t = (value1 + value2 + value3) / 2;
+            E = Math.Sqrt(t * (t - value1) - (t - value2) * (t - value3));
+
+            return E;
+            
+        }
+
+    }
+
+}
